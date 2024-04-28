91d10e37fe5d007e3056e7f8963cdd8e27b2cebb
Yordis Prieto <yordis.prieto@gmail.com>
2024-04-28 00:44:13 -0400

    feat: add digitalocean_certificate resource

--- apis/digitalocean/v1alpha1/zz_generated.managedlist.go
+++ apis/digitalocean/v1alpha1/zz_generated.managedlist.go
@@ -16,6 +16,15 @@ func (l *CdnList) GetItems() []resource.Managed {
 	return items
 }
 
+// GetItems of this CertificateList.
+func (l *CertificateList) GetItems() []resource.Managed {
+	items := make([]resource.Managed, len(l.Items))
+	for i := range l.Items {
+		items[i] = &l.Items[i]
+	}
+	return items
+}
+
 // GetItems of this TagList.
 func (l *TagList) GetItems() []resource.Managed {
 	items := make([]resource.Managed, len(l.Items))
